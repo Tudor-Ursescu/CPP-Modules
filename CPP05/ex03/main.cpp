f5405c2cf4130cf54681af8600cb1bc10504face
Tudor Ursescu <tursescu@student.42wolfsburg.de>
2025-03-18 18:41:26 +0100

    made better test for ex03

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: tursescu <tursescu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/12 14:07:51 by tursescu          #+#    #+#             */
-/*   Updated: 2025/03/18 18:18:40 by tursescu         ###   ########.fr       */
+/*   Updated: 2025/03/18 18:40:12 by tursescu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,22 +40,28 @@ int main()
         std::string formRequest[][2] = {
             {"shrubbery creation", "home"},
             {"robotomy request", "Chihuahua"},
-            {"shrubbery creation", "park"}
+            {"shrubbery creation", "park"},
+            {"presidential pardon", "Bill"},
+            {"bla bla bla ", "big useless target"}
         };
         //create an array for storring created forms
-        AForm* forms[3];
+        AForm* forms[5];
 
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             forms[i] = someRandomIntern.makeForm(formRequest[i][0], formRequest[i][1]);
             forms[i]->beSigned(theBoss);
             theBoss.executeForm(*forms[i]);
-        }
-
-        for (int i = 0; i < 3; i++) {
             delete forms[i];
         }
+        // for (int i = 0; i < 5; i++) {
+        //     delete forms[i];
+        // }
     } catch (std::exception& e) {
         std::cerr << RED << "Exception caught: " << e.what() << RESET << std::endl;
     }
     return 0;
 }
+
+// if there is an exception thrown right after we exit out of the for loop at 53,
+// then the AForms are not deleted trough the delete for loop, 
+// so we delete them directly after use
